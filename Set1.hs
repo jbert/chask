ade8ee1954dec853f138e340d2531964114d0e55
John Berthels <john.berthels@gmail.com>
2025-05-05 11:25:07 +0100

    S1C1 compiles - wrong value

--- Set1.hs
+++ Set1.hs
@@ -2,32 +2,55 @@ module Main where
 
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Base64 as BSB64
-import Data.Either
-import Data.Function
-import Numeric (readHex)
+import qualified Data.ByteString.Char8 as C8
+import Data.List.Split
+import Data.Word
 import Test.HUnit
+import Text.ParserCombinators.ReadP (string)
 
 c1input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
 c1expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
--- convert hex parse to Either fail/succeed
-processReadS :: [(a, String)] -> Either String a
-processReadS [] = Left "No parse"
-processReadS [(v, "")] = Right v
-processReadS [(v, s)] = Left ("Incomplete parse: " ++ s)
-processReadS l = Left ("Multiple matches: " ++ show (length l))
+hexDigitToNum :: Char -> Either String Word8
+hexDigitToNum '0' = Right 0
+hexDigitToNum '1' = Right 1
+hexDigitToNum '2' = Right 2
+hexDigitToNum '3' = Right 3
+hexDigitToNum '4' = Right 4
+hexDigitToNum '5' = Right 5
+hexDigitToNum '6' = Right 6
+hexDigitToNum '7' = Right 7
+hexDigitToNum '8' = Right 8
+hexDigitToNum '9' = Right 9
+hexDigitToNum 'a' = Right 10
+hexDigitToNum 'b' = Right 11
+hexDigitToNum 'c' = Right 12
+hexDigitToNum 'd' = Right 13
+hexDigitToNum 'e' = Right 14
+hexDigitToNum 'f' = Right 15
+hexDigitToNum c = Left ("Bad hex digit: " ++ [c])
 
-dehex :: String -> Either String B.ByteString
-dehex s = readHex s & processReadS
+pairToByte :: String -> Either String Word8
+pairToByte [a, b] = do
+  x <- hexDigitToNum a
+  y <- hexDigitToNum a
+  return $ x * 16 + y
+pairToByte l = Left "Can only handle two-element hex string"
 
-main = do
-  print "hello"
+stringToPairs :: String -> Either String [String]
+stringToPairs s = if even (length s) then Right (chunksOf 2 s) else Left "Odd length list"
 
-{-
-c1got = dehex c1input & BSB64.encode
+-- hexStringToByteString hs = stringToPairs hs >>= map pairToByte
 
-assertNoErr e = TestCase (assertEqual "No error" True (isRight e))
+fromHex :: String -> Either String B.ByteString
+fromHex hs =
+  fmap B.pack (stringToPairs hs >>= mapM pairToByte)
+
+toB64 :: B.ByteString -> String
+toB64 bs = C8.unpack $ BSB64.encode bs
+
+c1got = fmap toB64 (fromHex c1input)
 
 c1 (Left e) = TestCase (assertEqual ("Error" ++ show e) True False)
 c1 (Right got) = TestCase (assertEqual "Correct val" c1expected got)
@@ -41,4 +64,3 @@ c1 (Right got) = TestCase (assertEqual "Correct val" c1expected got)
 tests = TestList [TestLabel "C1" (c1 c1got)]
 
 main = runTestTTAndExit tests
--}
