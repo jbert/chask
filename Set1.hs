6dfc22fd3caa75939eda89ef0e644c90a902b78b
John Berthels <john.berthels@gmail.com>
2025-05-05 14:31:52 +0100

    S1C2 done

--- Set1.hs
+++ Set1.hs
@@ -1,17 +1,20 @@
 module Main where
 
+import Control.Monad
+import Data.Bits
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Base64 as BSB64
 import qualified Data.ByteString.Char8 as C8
 import Data.List.Split
 import Data.Word
+import Numeric
 import Test.HUnit
 import Text.ParserCombinators.ReadP (string)
 
 -- S1C1 - convert hex -> data, then data -> base64
-c1input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+c1_in = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
-c1expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+c1_expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
 {-
 
@@ -61,12 +64,49 @@ fromHex hs =
 toB64 :: B.ByteString -> String
 toB64 bs = C8.unpack $ BSB64.encode bs
 
-c1got = fmap toB64 (fromHex c1input)
+c1_got = fmap toB64 (fromHex c1_in)
 
 -- There is likely a better way to assert we didn't error?
 c1_test (Left e) = TestCase (assertEqual ("Error" ++ show e) True False)
-c1_test (Right got) = TestCase (assertEqual "Correct val" c1expected got)
+c1_test (Right got) = TestCase (assertEqual "Correct val" c1_expected got)
 
-set1_tests = TestList [TestLabel "C1" (c1_test c1got)]
+-- S1C2 - XOR two hex buffers, show as hex
+c2_in_a = "1c0111001f010100061a024b53535009181c"
+
+c2_in_b = "686974207468652062756c6c277320657965"
+
+c2_expected = "746865206b696420646f6e277420706c6179"
+
+toHex :: B.ByteString -> String
+toHex bs = concatMap (`showHex` "") (B.unpack bs)
+
+xorBS :: B.ByteString -> B.ByteString -> Either String B.ByteString
+xorBS a b =
+  let la = B.length a
+      lb = B.length b
+   in if la /= lb
+        then
+          Left ("Length mismatch: " ++ show la ++ " != " ++ show lb)
+        else
+          Right (B.pack (zipWith xor (B.unpack a) (B.unpack b)))
+
+c2_got :: Either String String
+c2_got = do
+  a <- fromHex c2_in_a
+  b <- fromHex c2_in_b
+  c <- xorBS a b
+  return $ toHex c
+
+-- in      join $ (fmap (\b -> fmap (`xorBS` b) ae) be)
+
+-- Find nicer way to assert against Either and/or reduce boilerplate
+c2_test (Left e) = TestCase (assertEqual ("Error" ++ show e) True False)
+c2_test (Right got) = TestCase (assertEqual "Correct val" c2_expected got)
+
+set1_tests =
+  TestList
+    [ TestLabel "C1" (c1_test c1_got),
+      TestLabel "C2" (c2_test c2_got)
+    ]
 
 main = runTestTTAndExit set1_tests
